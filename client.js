d6bdae6fb24d376a1ca760556f859494b915492b
Kyle Hagler <khagler.kh@gmail.com>
2020-05-23 16:55:15 -0500

    delete function seems to work

--- client.js
+++ client.js
@@ -36,7 +36,7 @@ let employees = [
 
         //intialize the table with our employees
    for (let employee of employees){
-    let rowElement = $('<tr></tr>');
+    let rowElement = $('<tr class = "row"></tr>');
     rowElement.append(`<td>${employee.firstName}</td>`);
     rowElement.append(`<td>${employee.lastName}</td>`);
     rowElement.append(`<td>${employee.idNum}</td>`);
@@ -48,24 +48,58 @@ let employees = [
 
 }//end for of loop
 
-$('.delete-button').on('click', function(event){
-    //clear only the row that the clicked button is in!
-    const element = event.target; //this is the button that fired this event
-    // $(element).parent().parent().remove(); THIS LINE WORKS goes find grandparents
-    $(element).parents('tr').remove();
-});
+
 
 
 // let rowElement = $('<tr></tr>'); //trying to add empty row at end
 // rowElement.append('<td></td>');
 // $('table tbody').append(rowElement);
 
-    }//end function setUp
+$('.delete-button').on('click', function(event){
+    //clear only the row that the clicked button is in!
+    const element = event.target; //this is the button that fired this event
+    
+    // $(element).parent().parent().remove(); THIS LINE WORKS goes find grandparents
+    
+    parentElement = $(element).parents('tr');
+    console.log(parentElement[0].innerHTML);
+    let stringId = parentElement[0].innerHTML;
+
+    let index = -1;
+    for (let i=0; i<employees.length;i++){
+        console.log(employees[i].idNum);
+        if (stringId.search(employees[i].idNum) >= 0){
+            console.log('match found');
+            index = i;
+            console.log(index);
+        } else{
+
+        } //end else
+        
+    }; //ends for loop
+
+    console.log(index);
+    console.log(employees[index]);
+    // let n = stringId.search(employees.idNum.toString());
+    // console.log(n);
+
+   
+  
+    
+    $(element).parents('tr').remove();
+    employees.splice(parentElement[index], 1);
+
+    
+
+}); //end delete click
+
+}//end function setUp
+
+function monthlyCost(){
+
+}//end function
 
 
-    function monthlyCost(){
-
-    }//end function
 
     $('#submit').on('click', function(e) { //submit button is clicked
         const firstName = $('#firstName').val();
@@ -74,11 +108,23 @@ $('.delete-button').on('click', function(event){
         const title =$('#title').val();
         const annualSalary =$('#annualSalary').val();
 
+        if (firstName != '' && lastName !='' && idNum != '' && title !='' && title != '' && annualSalary!=''){
+
         let newEmployee = {firstName, lastName, idNum, title, annualSalary};
         console.log(newEmployee);
 
         employees.push(newEmployee);
         console.log(employees);
+
+        setUp();
+        }
+        else {
+            alert('Input fields cannot be left blank before submitting');
+        }
     });
+
+    
+
+    
 
     $(document).ready(setUp);
